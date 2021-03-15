0a3b5c25096261596f56c8ea64bc6482ce9a1767
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-03-14 23:05:46 -0400

    2021-03-14

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { model } from "./model";
+import { templates } from "./templates";
+import "./styles/main.css";
+
+const $site = document.querySelector("#site"); // The variable $site is a simple variable, but came from the DOM.
+
+model.forEach((block) => {
+  // let html = "";
+  // if (block.type === "title") {
+  //   html = title(block);
+  // } else if (block.type === "text") {
+  //   html = text(block);
+  // } else if (block.type === "columns") {
+  //   html = columns(block);
+  // } else if (block.type === "image") {
+  //   html = image(block);
+  // }
+  // $site.insertAdjacentHTML("beforeend", html);
+
+  const toHTML = templates[block.type];
+  if (toHTML) {
+    $site.insertAdjacentHTML("beforeend", toHTML(block));
+  }
+});
---
+++ src/model.js
@@ -0,0 +1,7 @@
+import image from "./assets/image.png";
+export const model = [
+  { type: "title", value: "Hello World from JS!" },
+  { type: "text", value: "Here will be some text" },
+  { type: "columns", value: ["1111111", "2222222", "3333333"] },
+  { type: "image", value: image },
+];
