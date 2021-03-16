3b77ab06f2d5645d3bb4e0025fe6342408cc4486
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-03-15 23:26:27 -0400

    2021-03-15

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { model } from "./model";
 import { templates } from "./templates";
 import "./styles/main.css";
 
-const $site = document.querySelector("#site"); // The variable $site is a simple variable, but came from the DOM.
+const $site = document.querySelector("#site"); // The variable $site is a simple variable, but came from the DOM
 
 model.forEach((block) => {
   // let html = "";
